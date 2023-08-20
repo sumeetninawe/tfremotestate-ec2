3cdc2aa0de4cfb3aa1c99f248265b09a833d3fd8
Let's Do Tech <113388070+letsdotech@users.noreply.github.com>
2023-08-20 16:20:19 +0200

    init

--- main.tf
+++ main.tf
@@ -1,10 +1,36 @@
-resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
+# data "terraform_remote_state" "network" {
+#   backend = "s3"
+#   config = {
+#     bucket = "tfremotestate-vpc"
+#     key    = "state"        // Path to state file within this bucket
+#     region = "eu-central-1" // Change this to the appropriate region
+#   }
+# }
 
- tags = {
-   Name = var.name_tag,
- }
+# output subnet_ids {
+#   value = data.terraform_remote_state.network.outputs.subnets
+# }
+
+data "aws_subnets" "my_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-0cb1aa34b173b1bb6"] // replace this with your VPC id
+  }
+}
+
+resource "aws_instance" "my_vm" {
+  // for terraform_remote_state
+  // for_each      = toset(data.terraform_remote_state.network.outputs.subnets)
+  // for aws_subnets data source
+  for_each      = toset(data.aws_subnets.my_subnets.ids)
+  ami           = var.ami //Ubuntu AMI
+  instance_type = var.instance_type
+
+  subnet_id = each.key
+
+  tags = {
+    Name = var.name_tag,
+  }
 }
 
 data "terraform_remote_state" "vpc" {
--- provider.tf
+++ provider.tf
@@ -1,24 +1,15 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 5.10.0"
-   }
- }
-
-#  backend "s3" {
-#    bucket = "tfremotestate-ec2"
-#    key    = "state"
-#    region = "eu-central-1"
-#    dynamodb_table = "tfremotestate-ec2"
-#  }
-
-backend "remote" {
-    hostname     = "spacelift.io" 
-    organization = "letsdotech"    
-
-    workspaces {
-      name = "TFRemoteState-EC2" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.10.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "tfremotestate-ec2"
+    key            = "state"
+    region         = "eu-central-1"
+    dynamodb_table = "tfremotestate-ec2"
   }
 }
